fb5a3b2c5d241eb6eb86ee7007b442045cd94a82
Dani Donisa <dani.doni@gmail.com>
2021-07-12 12:26:58 +0200

    Refactor

--- src/api/app/jobs/report_to_scm_job.rb
+++ src/api/app/jobs/report_to_scm_job.rb
@@ -14,13 +14,13 @@ class ReportToScmJob < CreateJob
     event_package = Package.find_by_project_and_name(event.payload['project'], Package.striping_multibuild_suffix(event.payload['package']))
     return false if event_package.blank?
 
-    EventSubscriptionsFinder.new
-                            .for_scm_channel_with_token(event_type: event_type, event_package: event_package)
-                            .each do |event_subscription|
+    matched_event_subscriptions = EventSubscriptionsFinder.new
+                                                          .for_scm_channel_with_token(event_type: event_type, event_package: event_package)
+                                                          .select do |event_subscription|
       workflow_filter_service = Workflows::Filter.new(filters: event_subscription.payload.with_indifferent_access[:workflow_filters])
-
-      next unless workflow_filter_service.match?(event)
-
+      workflow_filter_service.match?(event)
+    end
+    matched_event_subscriptions.each do |event_subscription|
       SCMStatusReporter.new(event.payload,
                             event_subscription.payload,
                             event_subscription.token.scm_token,
