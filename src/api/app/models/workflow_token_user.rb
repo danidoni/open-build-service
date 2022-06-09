6b6971a4beadb8b7473557365a77b8f555f25759
Dani Donisa <dani.doni@gmail.com>
2022-06-09 13:04:08 +0200

    Update schema information in model

--- src/api/app/models/workflow_token_user.rb
+++ src/api/app/models/workflow_token_user.rb
@@ -2,3 +2,16 @@ class WorkflowTokenUser < ApplicationRecord
   belongs_to :user
   belongs_to :token, class_name: 'Token::Workflow'
 end
+
+# == Schema Information
+#
+# Table name: workflow_token_users
+#
+#  token_id :bigint           not null, indexed
+#  user_id  :bigint           not null, indexed
+#
+# Indexes
+#
+#  index_workflow_token_users_on_token_id  (token_id)
+#  index_workflow_token_users_on_user_id   (user_id)
+#
