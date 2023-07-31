f7f9544010f78738bdb78109cd0bf875df5f6bf3
Dani Donisa <dani.doni@gmail.com>
2023-07-31 13:20:08 +0200

    Fix Style/ConditionalAssignment cop

--- src/api/app/queries/event_subscriptions_finder.rb
+++ src/api/app/queries/event_subscriptions_finder.rb
@@ -1,10 +1,10 @@
 class EventSubscriptionsFinder
   def initialize(relation = EventSubscription.all, event_package_or_request:)
-    if event_package_or_request.is_a(Package)
-      @relation = relation.where(package: event_package_or_request)
-    else
-      @relation = relation.where(bs_request: event_package_or_request)
-    end
+    @relation = if event_package_or_request.is_a(Package)
+                  relation.where(package: event_package_or_request)
+                else
+                  relation.where(bs_request: event_package_or_request)
+                end
   end
 
   def for_scm_channel_with_token(event_type:)
