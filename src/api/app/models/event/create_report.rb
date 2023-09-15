19f22faecffc50ece5fbec62778b9ad21c559449
Lukas Krause <lkrause@suse.de>
2023-09-15 18:55:10 +0200

    Update src/api/app/models/event/create_report.rb
    Co-authored-by: Henne Vogelsang <hvogel@opensuse.org>

--- src/api/app/models/event/create_report.rb
+++ src/api/app/models/event/create_report.rb
@@ -1,6 +1,6 @@
 module Event
   class CreateReport < Base
-    receiver_roles :admin, :staff
+    receiver_roles :moderator
     self.description = 'Report for a project, package, comment, user or request has been created'
 
     payload_keys :id, :user_id, :reportable_id, :reportable_type
