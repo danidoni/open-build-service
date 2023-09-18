229b8f0d86c19ecdc15f792981cfc598de9d748d
Lukas Krause <lkrause@suse.de>
2023-09-18 12:47:11 +0200

    Update src/api/app/models/event/create_report.rb
    Co-authored-by: Henne Vogelsang <hvogel@opensuse.org>

--- src/api/app/models/event/create_report.rb
+++ src/api/app/models/event/create_report.rb
@@ -1,7 +1,7 @@
 module Event
   class CreateReport < Base
     receiver_roles :moderator
-    self.description = 'Report for a content has been created'
+    self.description = 'Report for inappropriate content has been created'
 
     payload_keys :id, :user_id, :reportable_id, :reportable_type, :reason
 
