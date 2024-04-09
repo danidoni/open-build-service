ad191564a7e520550f2a59d2438f649bdcef5a54
ncounter <dleidi@suse.com>
2024-04-09 15:44:03 +0200

    Drop useless parameters

--- src/api/app/components/workflow_run_request_action_filter_component.rb
+++ src/api/app/components/workflow_run_request_action_filter_component.rb
@@ -1,9 +1,7 @@
 class WorkflowRunRequestActionFilterComponent < ApplicationComponent
-  def initialize(filter_item:, request_action: '')
+  def initialize(filter_item:)
     super
 
-    @request_action = request_action
-    @generic_event_type = 'pull_request'
     @filter_options = ['all'] + SCMWebhook::ALLOWED_PULL_REQUEST_ACTIONS + SCMWebhook::ALLOWED_MERGE_REQUEST_ACTIONS
     @filter_item = filter_item
   end
