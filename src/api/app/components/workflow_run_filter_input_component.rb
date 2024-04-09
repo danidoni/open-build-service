e37fbae428550e2a877af94d88491202199319cf
ncounter <dleidi@suse.com>
2024-04-09 16:01:01 +0200

    Drop useless method

--- src/api/app/components/workflow_run_filter_input_component.rb
+++ src/api/app/components/workflow_run_filter_input_component.rb
@@ -10,10 +10,6 @@ class WorkflowRunFilterInputComponent < ApplicationComponent
     @selected_input_filter = selected_input_filter
   end
 
-  def css_for_filter_item
-    'active' if @selected_input_filter.present?
-  end
-
   def selected_filter_value
     @selected_input_filter
   end
