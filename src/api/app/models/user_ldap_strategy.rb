82ca1c77532efb4340dfcbc2bab199906b8001a2
Dani Donisa <dani.doni@gmail.com>
2025-01-27 14:39:42 +0100

    Autocorrect rubocop Style/HashSlice cop
    You can get more details on
    https://msp-greg.github.io/rubocop/RuboCop/Cop/Style/HashSlice.html

--- src/api/app/models/user_ldap_strategy.rb
+++ src/api/app/models/user_ldap_strategy.rb
@@ -207,8 +207,8 @@ class UserLdapStrategy
       dn = [dn].flatten.join(',')
       begin
         dn_components = dn.split(',').map { |n| n.strip.split('=') }
-        dn_uid = dn_components.select { |x, _| x == 'uid' }.map! { |_, y| y }
-        dn_path = dn_components.select { |x, _| x == 'dc' }.map! { |_, y| y }
+        dn_uid = dn_components.slice('uid').map! { |_, y| y }
+        dn_path = dn_components.slice('dc').map! { |_, y| y }
         upn = "#{dn_uid.fetch(0)}@#{dn_path.join('.')}"
       rescue StandardError
         # if we run into unexpected input just return an empty string
