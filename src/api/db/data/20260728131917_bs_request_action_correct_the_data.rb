2734f0c80cc3da8d319c7c70e6e993c323ca73f9
Lukas Krause <lkrause@suse.de>
2026-02-02 17:51:01 +0100

    Cleanup `bs_request_action` attributes after validations got introduced
    We cleaned up and corrected some validations performed on the
    `bs_request_action` models in
    https://github.com/openSUSE/open-build-service/pull/19178.
    After those validations proven to be correct, we can cleanup
    the database with a data migration.
    
    Co-authored-by: Jay Michalska <jay.michalska@suse.com>

---
+++ src/api/db/data/20260728131917_bs_request_action_correct_the_data.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+class BsRequestActionCorrectTheData < ActiveRecord::Migration[7.2]
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Rails/SkipsModelValidations
+  def up
+    BsRequestAction.where(type: 'add_role')
+      .where(
+        'source_project IS NOT NULL OR source_package IS NOT NULL OR ' \
+        'source_rev IS NOT NULL OR sourceupdate IS NOT NULL OR ' \
+        'target_releaseproject IS NOT NULL OR target_repository IS NOT NULL'
+      ).in_batches do |relation|
+      relation.update_all(
+        source_project: nil,
+        source_package: nil,
+        source_rev: nil,
+        sourceupdate: nil,
+        target_releaseproject: nil,
+        target_repository: nil
+      )
+    end
+
+    BsRequestAction.where(type: 'change_devel')
+      .where(
+        'group_name IS NOT NULL OR person_name IS NOT NULL OR ' \
+        'role IS NOT NULL OR source_rev IS NOT NULL OR ' \
+        'sourceupdate IS NOT NULL OR target_releaseproject IS NOT NULL OR ' \
+        'target_repository IS NOT NULL'
+      ).in_batches do |relation|
+      relation.update_all(
+        group_name: nil,
+        person_name: nil,
+        role: nil,
+        source_rev: nil,
+        sourceupdate: nil,
+        target_releaseproject: nil,
+        target_repository: nil
+      )
+    end
+
+    BsRequestAction.where(type: 'delete')
+      .where(
+        'source_project IS NOT NULL OR source_package IS NOT NULL OR ' \
+        'source_rev IS NOT NULL OR sourceupdate IS NOT NULL OR ' \
+        'group_name IS NOT NULL OR person_name IS NOT NULL OR ' \
+        'role IS NOT NULL OR target_releaseproject IS NOT NULL'
+      ).in_batches do |relation|
+      relation.update_all(
+        source_project: nil,
+        source_package: nil,
+        source_rev: nil,
+        sourceupdate: nil,
+        group_name: nil,
+        person_name: nil,
+        role: nil,
+        target_releaseproject: nil
+      )
+    end
+
+    BsRequestAction.where(type: 'maintenance_incident')
+      .where('group_name IS NOT NULL OR person_name IS NOT NULL OR role IS NOT NULL')
+      .in_batches do |relation|
+      relation.update_all(
+        group_name: nil,
+        person_name: nil,
+        role: nil
+      )
+    end
+
+    BsRequestAction.where(type: ['maintenance_release', 'release'])
+      .where('group_name IS NOT NULL OR person_name IS NOT NULL OR role IS NOT NULL OR target_releaseproject IS NOT NULL')
+      .in_batches do |relation|
+      relation.update_all(
+        group_name: nil,
+        person_name: nil,
+        role: nil,
+        target_releaseproject: nil
+      )
+    end
+
+    BsRequestAction.where(type: 'set_bugowner')
+      .where(
+        'source_project IS NOT NULL OR source_package IS NOT NULL OR ' \
+        'source_rev IS NOT NULL OR sourceupdate IS NOT NULL OR ' \
+        'role IS NOT NULL OR target_releaseproject IS NOT NULL OR ' \
+        'target_repository IS NOT NULL'
+      ).in_batches do |relation|
+      relation.update_all(
+        source_project: nil,
+        source_package: nil,
+        source_rev: nil,
+        sourceupdate: nil,
+        role: nil,
+        target_releaseproject: nil,
+        target_repository: nil
+      )
+    end
+
+    BsRequestAction.where(type: 'submit')
+      .where(
+        'group_name IS NOT NULL OR person_name IS NOT NULL OR ' \
+        'role IS NOT NULL OR target_releaseproject IS NOT NULL OR ' \
+        'target_repository IS NOT NULL'
+      ).in_batches do |relation|
+      relation.update_all(
+        group_name: nil,
+        person_name: nil,
+        role: nil,
+        target_releaseproject: nil,
+        target_repository: nil
+      )
+    end
+  end
+  # rubocop:enable Rails/SkipsModelValidations
+  # rubocop:enable Metrics/CyclomaticComplexity
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
